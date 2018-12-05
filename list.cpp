b6262613dd8375e59374c5c5b7aaaa14f3c658c7
Dana <43552905+catavasia@users.noreply.github.com>
2018-12-05 10:25:05 +0300

    Add files via upload

--- list.cpp
+++ list.cpp
@@ -1,55 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#define E(code, name); const int ERROR_##name = code;
-#define O(code, name); const int OK_##name = code;
-const int PTR_OK = 1;
-const int PTR_NULL = -1;
-
-struct Node
-{
-	int data;
-	Node* next;
-	Node* prev;
-};
-
+#include "node.h"
 struct List
 {
 	Node* head;
-	//Node* tail;
 	size_t size;
 };
 
-Node* CreateAndLinkNode(Node* , Node* , int ); // вставка между узлами
 List* CreateList(); //создание списка
-int CheckPtr(Node* cur);
-void PrintNode(Node* cur);
-//void DeleteNode( Node* cur);
-Node* CreateNode(int);
-Node* AddToHeadList(List* list, int data); // вставляет в начало списка
-Node* InsertNodeAfter(Node* cur, int data);
-void DeleteNodeBetween(Node* cur);
-void DeleteNodeFromHead(List* list, Node* cur);
-void DeleteNodeFromTail(Node* cur);
-void DeleteNode(List* list, Node* cur);
+void DeleteNodeFromList(List* list, Node* cur);
 Node* Search(List* list, int data);
-/*
-Node* InsertNodeBefore(Node* cur, int data);
-void Swap(Node* one, Node* two);
-Node* FindDataAfter(Node* begin, int data);
-Node* FindDataBefore(Node* begin, int data);
-Node* FindEverywhere(Node* begin, int data);
-//void InsertInHead(Node* cur, List* list);
-int CheckPtr(Node* cur);
-int NodeIndex(int data);
-void PrintSince(Node* begin);
-*/
+void AddToHeadList(List* list, Node* new_node);
+void DeleteList(List* list);
 int main()
 {
 	List* new_list = CreateList();
-	Node* new_node = AddToHeadList(new_list, 6);
-	Node* a = AddToHeadList(new_list, 7);
-	Node* b = AddToHeadList(new_list, 9);
+	
 	Node* cur = Search(new_list, 7);
 	printf("cur  %p\n", cur);
 	printf("new_node %p\n", new_node);
@@ -94,189 +58,47 @@ int main()
 	return 0;
 }
 
-List* CreateList()
-{
-	List* new_list = new List;
-	new_list->head = NULL;
-	//list->tail = NULL;
-	new_list->size = 0;
-	printf("list was created\n");
-	return new_list;
-}
-
-Node* AddToHeadList(List* list, int data)
-{
-	list->size++;
-	Node* new_node = CreateNode(data);
-	int check = CheckPtr(list->head);
-	if( check == PTR_NULL)
-	{
-		list->head = new_node;
-		return new_node;
-	}
-	list->head->prev = new_node;
-	new_node->next = list->head;
-	list->head = new_node;
-	return new_node;
-}
-/*
-Node* AddToTailList(List* list, int data)
-{
-	int check = CheckPtr(list->head);
-	if( check == PTR_NULL)
-	{
-		Node* new_node = CreateNode(data);
-		list->head = new_node;
-		return new_node;
-	}
-}
-*/	
-Node* CreateNode(int data)
-{
-	Node* new_node = (Node*)calloc(1, sizeof(Node));
-	new_node->prev = NULL;
-	new_node->next = NULL;
-	new_node->data = data;
-	return new_node;
-}
-
-
-/*
-Node* InsertNodeAfter(Node* cur, int data)
-{
-	Node* new_node = CreateNode(data);
-	cur->next = new_node;
-	new_node->prev = cur;
-	return new_node;
-}
-*/
-Node* CreateAndLinkNode(Node* prev, Node* next, int data)
-{
-	Node* new_node = CreateNode(data);
-	new_node->prev = prev;
-	next->prev = new_node;
-	new_node->next = next;
-	prev->next = new_node;
-	new_node->data = data;
-	return new_node;
-}
-
-void PrintNode(Node* cur)
-{
-	printf("%d\n", cur->data);
-}
-/*
-void PrintSince(Node* begin)
-{
-	Node* cur = begin;
-	while(!cur)
-	{
-		PrintNode(cur);
-		cur = cur->next;
-	}
-}
-*/
-
-void DeleteNodeFromHead(List* list, Node* cur)
+void DeleteNodeFromList(List* list, Node* cur)
 {
 	list->size--;
-	list->head = cur->next;
-	cur->next->prev = NULL;
-	free(cur);
-}
-
-void DeleteNodeFromTail(Node* cur)
-{
-	cur->prev->next = NULL;
-	free(cur);
-}
-
-void DeleteNodeBetween(Node* cur)
-{
+	if(cur->next == NULL && cur->prev != NULL)
+	{
+		cur->prev->next = NULL;
+		delete cur;
+		printf("last node was deleted\n");
+		return;
+	}
+	if(cur->prev == NULL && cur->next != NULL)
+	{
+		list->head = cur->next;
+		cur->next->prev = NULL;
+		printf("first node was deleted\n");
+		delete cur;
+		return;
+	}
 	cur->prev->next = cur->next;
 	cur->next->prev = cur->prev;
-	free(cur);
+	delete cur;
 	printf("Cur was deleted\n");
-	
-}
-
-void DeleteNode(List* list, Node* cur)
-{
-	if (CheckPtr(cur->prev)== PTR_NULL)
-		DeleteNodeFromHead(list, cur);
-	if (CheckPtr(cur->next)== PTR_NULL)
-		DeleteNodeFromTail(cur);
-	else
-		DeleteNodeBetween(cur);
-	list->size--;
 }
 
 Node* Search(List* list, int data)
 {
 	Node* cur = list->head;
-	while(!cur)
-	{
-		if(cur->data == data)
-			return cur;
-		cur = cur->next;
-		printf("ptr is %p\n", cur);
-	}
+	Node* found = FindDataAfter(cur, data);
+	printf("ptr is %p\n", found);
 	return cur;
 }
-/*
-Node* InsertNodeBefore(Node* cur, int data)
-{
-	Node* new_node = CreateNode();
-	new_node->data = data;
-	cur->prev->next = new_node;
-	new_node->prev = cur->prev;
-	cur->prev = new_node;
-	new_node->next = cur;
-	return new_node;
-}
 
-void Swap(Node* one, Node* two)
+void DeleteList(List* list)
 {
-	int c = one->data;
-	one->data = two->data;
-	two->data = c;
-}
-
-Node* FindDataAfter(Node* begin, int data)
-{
-	Node* cur = begin;
-	while(!cur)
+	Node* cur = list->head;
+	while(cur!=NULL)
 	{
-		if(cur->data == data)
-			return cur;
+		DeleteNodeFromList(list, cur);
 		cur = cur->next;
 	}
-	return cur;
 }
-
-Node* FindDataBefore(Node* begin, int data)
-{
-	Node* cur = begin;
-	while(!cur)
-	{
-		if(cur->data == data)
-			return cur;
-		cur = cur->prev;
-	}
-	return cur;
-}
-
-Node* SearchEverywhere(Node* begin, int data)
-{
-	Node* cur_before = FindDataBefore(begin,data);
-	if( cur_before!= NULL)
-		return cur_before;
-	Node* cur_after = FindDataBefore(begin,data);
-	if( cur_after!= NULL)
-		return cur_after;
-	return NULL;
-}
-*/
 int CheckPtr(Node* cur)
 {
 	if(cur == NULL)
@@ -284,13 +106,3 @@ int CheckPtr(Node* cur)
 	else
 		return PTR_OK;
 }
-	
-//int NodeIndex(Node* n)
-
-	
-	
-	
-
-
-	
-
--- node.cpp
+++ node.cpp
@@ -1,154 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#define E(code, name); const int ERROR_##name = code;
-#define O(code, name); const int OK_##name = code;
-const int PTR_OK = 1;
-const int PTR_ERROR = -1;
-
-struct Node
+Node* CreateNode(int data)
 {
-	int data;
-	Node* next;
-	Node* prev;
-};
-struct List
-{
-	Node* head;
-	//Node* tail;
-	size_t size;
-};
-
-Node* CreateAndLinkNode(Node* , Node* , int );
-void PrintNode(Node* cur);
-void DeleteNode( Node* cur);
-Node* CreateNode();
-Node* InsertAfter(Node* cur, int data);
-void DeleteNode(Node* cur);
-Node* InsertBefore(Node* cur, int data);
-void Swap(Node* one, Node* two);
-Node* FindDataAfter(Node* begin, int data);
-Node* FindDataBefore(Node* begin, int data);
-Node* FindEverywhere(Node* begin, int data);
-List* CreateList();
-//void InsertInHead(Node* cur, List* list);
-int CheckPtr(Node* cur);
-void PrintList(List* list);
-Node* PutData(Node* cur, int data);
-void AddToList(List* list, int data);
-int NodeIndex(int data);
-
-
-int main()
-{
-	Node* n = (Node*)calloc(1, sizeof(Node));
-	n->data = 4;
-	Node* a = (Node*)calloc(1, sizeof(Node));
-	a->data = 13;
-	List* list = CreateList();
-	printf("%lu\n", list->size);
-	AddToList(list, 5);
-	AddToList(list, 9);
-	
-	printf("size is %lu\n", list->size);
-	printf(" head is %p\n", list->head);
-	PrintList(list);
-	/*printf("n prev  %p\n", n->prev);
-	printf("n next  %p\n", n->next);
-	//printf("OK\n");
-	printf(" a prev %p\n", a->prev);
-	printf("a next  %p\n", a->next);
-	printf(" n prev %p\n", n->prev);
-	printf("n next  %p\n", n->next);
-	PrintList(n);
-	/*printf("size of int is %lu\n", sizeof(int));
-	printf("size of pointer is %lu\n", sizeof(int*));
-	printf("size of struct is %lu\n", sizeof(Node));
-	List* list = CreateList();
-	printf("list head %p\n", list->head);
-	printf("list tail %p\n", list->tail);
-	AddtoList(n,list);
-	printf(" pointer is %p\n", n);
-	printf("next  %p\n", n->next);
-	printf("list tail %p\n", list->tail);
-	printf("list head %p\n", list->head);
-	printf("list tail %p\n", list->tail);
-	//Swap(n,a);
-	//PrintNode(n);
-	//PrintNode(a);
-	Node* b = CreateAndLinkNode(n, a, 55);
-	PrintNode(b);
-	Node* c = InsertAfter(b, 33);
-	PrintNode(c);
-	printf("%p\n", b->next);
-	printf("%p\n", c);
-	printf("%p\n", a->prev);
-	printf("%p\n",c);
-	DeleteNode(c);
-	printf("%p\n", b->next);
-	printf("%p\n", a);
-	Node* h = InsertBefore(b, 88);
-	PrintNode(h);
-	printf("%p\n", h->next);
-	printf("%p\n", b);
-	*/
-	//int check = CheckPtr(n);
-	//printf("check is %d", check);
-	return 0;
-}
-
-List* CreateList()
-{
-	List* list = new List;
-	list->head = NULL;
-	//list->tail = NULL;
-	list->size = 0;
-	printf("list was created\n");
-	return list;
-}
-
-void AddToList(List* list, int data)
-{
-	Node* new_node = CreateNode();
-	list->head = new_node;
-	new_node = PutData(new_node, data);
-	list->size++;
-}
-/*void InsertInHead(Node* cur, List* list)
-{
-	if(list->head != NULL)
-		InsertAfter(list->head);
-	
-	list->head = cur;
-}
-
-void InsertInTail(Node* cur, List* list)
-{
-	list->tail->next = cur;
-	cur->prev = list->tail;
-	cur->next = list->tail;
-	list->tail = cur;
-}
-	
-*/
-
-Node* PutData(Node* cur, int data)
-{
-	cur->data = data;
-	return cur;
-}
-
-Node* CreateNode()
-{
-	Node* new_node = (Node*)calloc(1, sizeof(Node));
+	Node* new_node = new Node;
+	int check = CheckPtr(new_node);
+	if(check == PTR_NULL)
+	{
+		printf("new node ptr is NULL\n");
+		return NULL;
+	}
 	new_node->prev = NULL;
 	new_node->next = NULL;
-	new_node->data = 0;
+	new_node->data = data;
 	return new_node;
 }
+
 Node* CreateAndLinkNode(Node* prev, Node* next, int data)
 {
-	Node* new_node = (Node*)calloc(1, sizeof(Node));
+	Node* new_node = CreateNode(data);
 	new_node->prev = prev;
 	next->prev = new_node;
 	new_node->next = next;
@@ -159,47 +26,14 @@ Node* CreateAndLinkNode(Node* prev, Node* next, int data)
 
 void PrintNode(Node* cur)
 {
-	printf("%d\n", cur->data);
+	printf("data %d\n", cur->data);
 }
 
-void PrintList(List* list)
+Node* InsertNodeAfter(Node* cur, int data)
 {
-	Node* cur = list->head;
-	while(!cur)
-	{
-		PrintNode(cur);
-		printf("Node was printed\n");
-		cur = cur->next;
-	}
-
-}
-void DeleteNode(Node* cur)
-{
-	cur->prev->next = cur->next;
-	cur->next->prev = cur->prev;
-	free(cur);
-	//printf("Cur was deleted\n");
-}
-
-Node* InsertAfter(Node* cur, int data)
-{
-	Node* new_node = CreateNode();
-	new_node->data = data;
-	cur->next->prev = new_node;
-	new_node->next = cur->next;
+	Node* new_node = CreateNode(data);
 	cur->next = new_node;
 	new_node->prev = cur;
-	return new_node;
-}
-
-Node* InsertDataBefore(Node* cur, int data)
-{
-	Node* new_node = CreateNode();
-	new_node->data = data;
-	cur->prev->next = new_node;
-	new_node->prev = cur->prev;
-	cur->prev = new_node;
-	new_node->next = cur;
 	return new_node;
 }
 
@@ -219,6 +53,7 @@ Node* FindDataAfter(Node* begin, int data)
 			return cur;
 		cur = cur->next;
 	}
+	return cur;
 }
 
 Node* FindDataBefore(Node* begin, int data)
@@ -230,33 +65,61 @@ Node* FindDataBefore(Node* begin, int data)
 			return cur;
 		cur = cur->prev;
 	}
+	return cur;
 }
-
-Node* SearchEverywhere(Node* begin, int data)
+Node* FindEverywhere(Node* begin, int data)
 {
 	Node* cur_before = FindDataBefore(begin,data);
 	if( cur_before!= NULL)
 		return cur_before;
-	Node* cur_after = FindDataBefore(begin,data);
+	Node* cur_after = FindDataAfter(begin,data);
 	if( cur_after!= NULL)
 		return cur_after;
 	return NULL;
 }
 
+void PrintSince(Node* begin)
+{
+	Node* cur = begin;
+	while(!cur)
+	{
+		PrintNode(cur);
+		cur = cur->next;
+	}
+}
+
+void DeleteNode(Node* cur)
+{
+	if(cur->next == NULL && cur->prev != NULL)
+	{
+		cur->prev->next = NULL;
+		delete cur;
+		printf("last node was deleted\n");
+		return;
+	}
+	if(cur->prev == NULL && cur->next != NULL)
+	{
+		cur->next->prev = NULL;
+		printf("first node was deleted\n");
+		delete cur;
+		return;
+	}
+	if(cur->prev == NULL && cur->next == NULL)
+	{
+		delete cur;	
+		printf("lonely node was deleted\n");
+		return;
+	}
+	cur->prev->next = cur->next;
+	cur->next->prev = cur->prev;
+	delete cur;
+	printf("Cur was deleted\n");
+}
+
 int CheckPtr(Node* cur)
 {
 	if(cur == NULL)
-		return PTR_ERROR;
-	return PTR_OK;
+		return PTR_NULL;
+	else
+		return PTR_OK;
 }
-	
-//int NodeIndex(Node* n)
-
-	
-
-		
-	
-
-
-	
-
